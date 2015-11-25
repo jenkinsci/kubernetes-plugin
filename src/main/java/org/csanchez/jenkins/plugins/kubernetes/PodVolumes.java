1e8a547935ad8df510fe9a044e9f8f6227fcbf68
Ioannis Canellos <iocanel@gmail.com>
2015-11-25 22:16:25 +0200

    Add support for secrets and empty dir volumes.

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/PodVolumes.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/PodVolumes.java
@@ -19,6 +19,82 @@ public class PodVolumes {
         public abstract Volume buildVolume(String volumeName);
     }
 
+    public static class EmptyDirVolume extends PodVolume {
+
+        private static final String DEFAULT_MEDIUM = "";
+        private static final String MEMORY_MEDIUM = "Memory";
+
+        private String mountPath;
+        private Boolean memory;
+
+        @DataBoundConstructor
+        public EmptyDirVolume(String mountPath, Boolean memory) {
+            this.mountPath = mountPath;
+            this.memory = memory;
+        }
+
+        @Override
+        public String getMountPath() {
+            return mountPath;
+        }
+
+        public String getMedium() {
+           return memory ? MEMORY_MEDIUM : DEFAULT_MEDIUM;
+        }
+
+        @Override
+        public Volume buildVolume(String volumeName) {
+            return new VolumeBuilder().withName(volumeName)
+                    .withNewEmptyDir(getMedium())
+                    .build();
+        }
+
+        @Extension
+        public static class DescriptorImpl extends Descriptor<PodVolume> {
+            @Override
+            public String getDisplayName() {
+                return "Empty Dir Volume";
+            }
+        }
+    }
+
+    public static class SecretVolume extends PodVolume {
+
+        private String mountPath;
+        private String secretName;
+
+        @DataBoundConstructor
+        public SecretVolume(String mountPath, String secretName) {
+            this.mountPath = mountPath;
+            this.secretName = secretName;
+        }
+
+        @Override
+        public Volume buildVolume(String volumeName) {
+            return new VolumeBuilder()
+                    .withName(volumeName)
+                    .withNewSecret(getSecretName())
+                    .build();
+        }
+
+        public String getSecretName() {
+            return secretName;
+        }
+
+        @Override
+        public String getMountPath() {
+            return mountPath;
+        }
+
+        @Extension
+        public static class DescriptorImpl extends Descriptor<PodVolume> {
+            @Override
+            public String getDisplayName() {
+                return "Secret Volume";
+            }
+        }
+    }
+
     public static class HostPathVolume extends PodVolume {
         private String mountPath;
         private String hostPath;
