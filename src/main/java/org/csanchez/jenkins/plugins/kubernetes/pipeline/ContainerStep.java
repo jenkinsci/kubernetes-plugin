2be70ba43db9c00ab872e8d2c1eac7db27f084e0
Jefferson Girao <jefferson.girao@klarna.com>
2016-12-06 16:15:11 +0100

    Instantiating Kubernetes Client with proper config in Container Steps

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/ContainerStep.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/ContainerStep.java
@@ -5,6 +5,7 @@ import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 import java.io.Serializable;
 
@@ -13,6 +14,10 @@ public class ContainerStep extends AbstractStepImpl implements Serializable {
     private static final long serialVersionUID = 5588861066775717487L;
 
     private final String name;
+
+    private static final String DEFAULT_CLOUD = "kubernetes";
+
+    private String cloud = DEFAULT_CLOUD;
 
     @DataBoundConstructor
     public ContainerStep(String name) {
@@ -23,10 +28,19 @@ public class ContainerStep extends AbstractStepImpl implements Serializable {
         return name;
     }
 
+    public String getCloud() {
+        return cloud;
+    }
+
+    @DataBoundSetter
+    public void setCloud(String cloud) {
+        this.cloud = cloud;
+    }
+
     @Extension
     public static class DescriptorImpl extends AbstractStepDescriptorImpl {
 
-       public DescriptorImpl() {
+        public DescriptorImpl() {
             super(ContainerStepExecution.class);
         }
 
