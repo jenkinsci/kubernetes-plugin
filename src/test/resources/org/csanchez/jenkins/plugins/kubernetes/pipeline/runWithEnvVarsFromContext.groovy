9137c5881c295bcbe9ca5375135c330bab343593
Scott Hebert <shebert@redhat.com>
2017-08-20 21:31:26 -0400

    Additional tests for environment variables in containers
    - Adds to #204

---
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/runWithEnvVarsFromContext.groovy
@@ -0,0 +1,21 @@
+podTemplate(cloud: 'kubernetes-plugin-test', label: 'mypod',
+    containers: [
+        containerTemplate(name: 'busybox', image: 'busybox', ttyEnabled: true, command: '/bin/cat'),
+    ]) {
+
+    env.FROM_ENV_DEFINITION = "ABC"
+    node ('mypod') {
+        stage('Run busybox') {
+            withEnv(['FROM_WITHENV_DEFINITION=DEF']) {
+                container('busybox') {
+                    sh 'echo inside container'
+                    sh """
+                    echo The value of FROM_ENV_DEFINITION is \$FROM_ENV_DEFINITION
+                    echo The value of FROM_WITHENV_DEFINITION is \$FROM_WITHENV_DEFINITION
+                    echo
+                    """
+                }
+            }
+        }
+    }
+}
