da505eac8a3e7089250fcc15adf1daffd4ca0b4f
Vincent Latombe <vincent@latombe.net>
2017-09-11 09:02:19 +0200

    Flag unused constructor as deprecated

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesSlave.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesSlave.java
@@ -54,6 +54,7 @@ public class KubernetesSlave extends AbstractCloudSlave {
         return template;
     }
 
+    @Deprecated
     public KubernetesSlave(PodTemplate template, String nodeDescription, KubernetesCloud cloud, String labelStr)
             throws Descriptor.FormException, IOException {
 
