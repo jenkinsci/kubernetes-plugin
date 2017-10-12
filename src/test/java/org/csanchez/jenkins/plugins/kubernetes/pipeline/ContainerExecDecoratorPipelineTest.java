407082d8b387b58fbb25e694b413a0aaf72963d3
Carlos Sanchez <carlos@apache.org>
2017-10-12 12:30:27 +0200

    [JENKINS-47225] Add test for proc starter

---
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/ContainerExecDecoratorPipelineTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017 Original Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.csanchez.jenkins.plugins.kubernetes.pipeline;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
+
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.PrivateKeySource;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+
+public class ContainerExecDecoratorPipelineTest extends AbstractKubernetesPipelineTest {
+
+    @Issue({ "JENKINS-47225", "JENKINS-42582" })
+    @Test
+    public void sshagent() throws Exception {
+        PrivateKeySource source = new BasicSSHUserPrivateKey.DirectEntryPrivateKeySource(
+                new String(IOUtils.toByteArray(getClass().getResourceAsStream("id_rsa"))));
+        BasicSSHUserPrivateKey credentials = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,
+                "ContainerExecDecoratorPipelineTest-sshagent", "bob", source, "", "test credentials");
+        SystemCredentialsProvider.getInstance().getCredentials().add(credentials);
+
+        WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "sshagent");
+        p.setDefinition(new CpsFlowDefinition(loadPipelineScript("sshagent.groovy"), true));
+        WorkflowRun b = p.scheduleBuild2(0).waitForStart();
+        assertNotNull(b);
+        r.waitForCompletion(b);
+        r.assertLogContains("Identity added:", b);
+    }
+}
---
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/sshagent.groovy
@@ -0,0 +1,13 @@
+podTemplate(label: 'mypod', containers: [
+        containerTemplate(name: 'ssh-client', image: 'kroniak/ssh-client:3.6', ttyEnabled: true, command: 'cat')
+]) {
+    node ('mypod') {
+        stage('container log') {
+            container('ssh-client') {
+                sshagent (credentials: ['ContainerExecDecoratorPipelineTest-sshagent']) {
+                    sh 'ssh -vT -o "StrictHostKeyChecking=no" git@github.com'
+                }
+            }
+        }
+    }
+}
