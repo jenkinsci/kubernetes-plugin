5e74784737481cd4336228bf275363ec5f6274e2
David Joaquim <david.joaquim@gmail.com>
2017-10-20 08:24:37 +0200

    Annotate the build pod with task status on status change.

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesComputer.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesComputer.java
@@ -13,22 +13,25 @@ import java.util.logging.Logger;
  * @author Carlos Sanchez carlos@apache.org
  */
 public class KubernetesComputer extends AbstractCloudComputer<KubernetesSlave> {
-    private static final Logger LOGGER = Logger.getLogger(KubernetesComputer.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(KubernetesComputer.class.getName());
 
-    public KubernetesComputer(KubernetesSlave slave) {
+	private static final String TASK_NAME_POD_ANNOTATION = "jenkins.task.name";
+	private static final String TASK_STATUS_POD_ANNOTATION = "jenkins.task.status";
+
+	public KubernetesComputer(KubernetesSlave slave) {
         super(slave);
     }
 
     @Override
     public void taskAccepted(Executor executor, Queue.Task task) {
-        annotatePodWithTaskName(task);
+        annotatePodWithTaskInfo(task, "accepted");
         super.taskAccepted(executor, task);
         LOGGER.fine(" Computer " + this + " taskAccepted");
     }
 
     @Override
     public void taskCompleted(Executor executor, Queue.Task task, long durationMS) {
-        cleanupPodAnnotation();
+        annotatePodWithTaskInfo(task, "completed");
         LOGGER.log(Level.FINE, " Computer " + this + " taskCompleted");
 
         // May take the slave offline and remove it, in which case getNode()
@@ -38,7 +41,7 @@ public class KubernetesComputer extends AbstractCloudComputer<KubernetesSlave> {
 
     @Override
     public void taskCompletedWithProblems(Executor executor, Queue.Task task, long durationMS, Throwable problems) {
-        cleanupPodAnnotation();
+        annotatePodWithTaskInfo(task, "completedWithProblems");
         super.taskCompletedWithProblems(executor, task, durationMS, problems);
         LOGGER.log(Level.FINE, " Computer " + this + " taskCompletedWithProblems");
     }
@@ -48,37 +51,21 @@ public class KubernetesComputer extends AbstractCloudComputer<KubernetesSlave> {
         return String.format("KubernetesComputer name: %s slave: %s", getName(), getNode());
     }
 
-    private void annotatePodWithTaskName(Queue.Task task) {
+    private void annotatePodWithTaskInfo(Queue.Task task, String status) {
         KubernetesClient k8sClient = null;
         try {
             KubernetesSlave slave = getNode();
             if (slave != null) {
                 k8sClient = slave.getKubernetesCloud().connect();
-                Pod done = k8sClient.pods().withName(slave.getNodeName()).edit().editMetadata()
-                        .addToAnnotations("jenkins.task.name", task.getName()).endMetadata().done();
+                Pod pod = k8sClient.pods().withName(slave.getNodeName()).edit()
+                        .editMetadata()
+                        .addToAnnotations(TASK_NAME_POD_ANNOTATION, task.getName())
+                        .addToAnnotations(TASK_STATUS_POD_ANNOTATION, status)
+                        .endMetadata().done();
 
-                String nodeName = done.getSpec().getNodeName();
-                LOGGER.info("accepted task [" + task.getName() + "] in pod [" + slave.getNodeName() + "] on node [" + nodeName + "]");
-            }
-        }
-        catch (Exception e) {
-            LOGGER.log(Level.WARNING, "Cannot contact k8s server", e);
-        }
-        finally {
-            if (k8sClient != null) {
-                k8sClient.close();
-            }
-        }
-    }
-
-    private void cleanupPodAnnotation() {
-        KubernetesClient k8sClient = null;
-        try {
-            KubernetesSlave slave = getNode();
-            if (slave != null) {
-                k8sClient = slave.getKubernetesCloud().connect();
-                k8sClient.pods().withName(slave.getNodeName()).edit().editMetadata()
-                        .removeFromAnnotations("jenkins.task.name").endMetadata().done();
+                String nodeName = pod.getSpec().getNodeName();
+                LOGGER.info("task [" + task.getName() + "] updated with status [" + status + "] in pod [" + slave.getNodeName() + "] on node ["
+                        + nodeName + "]");
             }
         }
         catch (Exception e) {
