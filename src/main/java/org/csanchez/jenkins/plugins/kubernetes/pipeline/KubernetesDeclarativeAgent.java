11542b4cc6cf81432c97adfd79fd08ac64cb1a62
James Strachan <james.strachan@gmail.com>
2017-11-21 16:18:04 +0000

    allow a podTemplate to be configured by name in declarative pipelines
    fixes JENKINS-48140

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgent.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgent.java
@@ -1,7 +1,16 @@
 package org.csanchez.jenkins.plugins.kubernetes.pipeline;
 
+import hudson.model.labels.LabelAtom;
+import hudson.slaves.Cloud;
+import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
 import org.csanchez.jenkins.plugins.kubernetes.ContainerTemplate;
+import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud;
+import org.csanchez.jenkins.plugins.kubernetes.PodAnnotation;
+import org.csanchez.jenkins.plugins.kubernetes.PodImagePullSecret;
+import org.csanchez.jenkins.plugins.kubernetes.PodTemplate;
+import org.csanchez.jenkins.plugins.kubernetes.model.TemplateEnvVar;
+import org.csanchez.jenkins.plugins.kubernetes.volumes.PodVolume;
 import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgent;
 import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgentDescriptor;
@@ -10,7 +19,9 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDeclarativeAgent> {
@@ -26,11 +37,12 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
     private int activeDeadlineSeconds;
 
     private ContainerTemplate containerTemplate;
+    private PodTemplate podTemplate;
+    private String podTemplateName;
 
     @DataBoundConstructor
-    public KubernetesDeclarativeAgent(String label, ContainerTemplate containerTemplate) {
+    public KubernetesDeclarativeAgent(String label) {
         this.label = label;
-        this.containerTemplate = containerTemplate;
     }
 
     public String getLabel() {
@@ -95,6 +107,24 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
         return containerTemplate;
     }
 
+    @DataBoundSetter
+    public void setContainerTemplate(ContainerTemplate containerTemplate) {
+        this.containerTemplate = containerTemplate;
+    }
+
+    public PodTemplate getPodTemplate() {
+        return podTemplate;
+    }
+
+    @DataBoundSetter
+    public void setPodTemplate(PodTemplate podTemplate) {
+        this.podTemplate = podTemplate;
+        List<ContainerTemplate> containers = podTemplate.getContainers();
+        if (containers != null && containers.size() > 0) {
+            this.containerTemplate = containers.get(0);
+        }
+    }
+
     public int getActiveDeadlineSeconds() {
         return activeDeadlineSeconds;
     }
@@ -102,13 +132,99 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
     @DataBoundSetter
     public void setActiveDeadlineSeconds(int activeDeadlineSeconds) { this.activeDeadlineSeconds = activeDeadlineSeconds; }
 
+    public String getPodTemplateName() {
+        return podTemplateName;
+    }
+
+    @DataBoundSetter
+    public void setPodTemplateName(String podTemplateName) {
+        this.podTemplateName = podTemplateName;
+        if (!StringUtils.isEmpty(podTemplateName)) {
+            String cloudName = getCloud();
+            if (StringUtils.isEmpty(cloudName)) {
+                cloudName = "kubernetes";
+            }
+            Cloud cloud = Jenkins.getInstance().getCloud(cloudName);
+            if (cloud instanceof KubernetesCloud) {
+                KubernetesCloud kubernetesCloud = (KubernetesCloud) cloud;
+                List<PodTemplate> templates = kubernetesCloud.getTemplates();
+                PodTemplate template = null;
+                for (PodTemplate t : templates) {
+                    String name = t.getName();
+                    if (podTemplateName.equals(name)) {
+                        template = t;
+                        break;
+                    }
+                }
+                if (template != null) {
+                    setPodTemplate(template);
+                }
+            }
+        }
+    }
+
     public Map<String,Object> getAsArgs() {
         Map<String,Object> argMap = new TreeMap<>();
 
+        int instanceCap = this.instanceCap;
+        int activeDeadlineSeconds = this.activeDeadlineSeconds;
+        String inheritFrom = this.inheritFrom;
+        String nodeSelector = this.nodeSelector;
+        String serviceAccount = this.serviceAccount;
+
+        String label = this.label;
+        if (podTemplate != null) {
+            if (StringUtils.isEmpty(label)) {
+                label = podTemplate.getLabel();
+                if (StringUtils.isEmpty(label)) {
+                    label = podTemplate.getName();
+                }
+            }
+            // override properties if specified on the podTemplate
+            if (podTemplate.getInstanceCap() > 0) {
+                instanceCap = podTemplate.getInstanceCap();
+            }
+            if (podTemplate.getActiveDeadlineSeconds() > 0) {
+                activeDeadlineSeconds = podTemplate.getActiveDeadlineSeconds();
+            }
+            if (!StringUtils.isEmpty(podTemplate.getInheritFrom())) {
+                inheritFrom = podTemplate.getInheritFrom();
+            }
+            if (!StringUtils.isEmpty(podTemplate.getNodeSelector())) {
+                nodeSelector = podTemplate.getNodeSelector();
+            }
+            if (!StringUtils.isEmpty(podTemplate.getServiceAccount())) {
+                serviceAccount = podTemplate.getServiceAccount();
+            }
+
+
+            argMap.put("containers", podTemplate.getContainers());
+
+            List<PodAnnotation> annotations = podTemplate.getAnnotations();
+            if (annotations != null && annotations.size() > 0) {
+                argMap.put("annotations", annotations);
+            }
+            Set<LabelAtom> labelSet = podTemplate.getLabelSet();
+            if (labelSet != null && labelSet.size() > 0) {
+                argMap.put("labelSet", labelSet);
+            }
+            List<TemplateEnvVar> envVars = podTemplate.getEnvVars();
+            if (envVars != null && envVars.size() > 0) {
+                argMap.put("envVars", envVars);
+            }
+            List<PodImagePullSecret> imagePullSecrets = podTemplate.getImagePullSecrets();
+            if (imagePullSecrets != null && imagePullSecrets.size() > 0) {
+                argMap.put("imagePullSecrets", imagePullSecrets);
+            }
+            List<PodVolume> volumes = podTemplate.getVolumes();
+            if (volumes != null && volumes.size() > 0) {
+                argMap.put("volumes", volumes);
+            }
+        } else if (containerTemplate != null) {
+            argMap.put("containers", Collections.singletonList(containerTemplate));
+        }
         argMap.put("label", label);
         argMap.put("name", label);
-        argMap.put("containers", Collections.singletonList(containerTemplate));
-
         if (!StringUtils.isEmpty(cloud)) {
             argMap.put("cloud", cloud);
         }
@@ -127,11 +243,9 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
         if (activeDeadlineSeconds != 0) {
             argMap.put("activeDeadlineSeconds", activeDeadlineSeconds);
         }
-
         if (instanceCap > 0) {
             argMap.put("instanceCap", instanceCap);
         }
-
         return argMap;
     }
 
