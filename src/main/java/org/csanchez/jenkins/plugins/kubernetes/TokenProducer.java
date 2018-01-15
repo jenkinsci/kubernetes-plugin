99fedd036a211bfefbc638744992e873cc20e6ee
Carlos Sanchez <carlos@apache.org>
2018-01-15 14:08:21 +0100

    Ignore findbugs errors

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/TokenProducer.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/TokenProducer.java
@@ -6,6 +6,6 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 @Deprecated
-@SuppressFBWarnings("NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
+@SuppressFBWarnings("NM_SAME_SIMPLE_NAME_AS_INTERFACE")
 public interface TokenProducer extends org.jenkinsci.plugins.kubernetes.credentials.TokenProducer {
 }
