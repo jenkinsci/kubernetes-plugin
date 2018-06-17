495cb6cf6c5b4a1cd7fb20dedec815913dca843d
Carlos Sanchez <carlos@apache.org>
2018-06-17 18:47:14 +0200

    Update example to latest kaniko

--- examples/kaniko.groovy
+++ examples/kaniko.groovy
@@ -8,36 +8,43 @@
  * https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/#create-a-secret-in-the-cluster-that-holds-your-authorization-token
  */
 
- def label = "kaniko-${UUID.randomUUID().toString()}"
+def label = "kaniko-${UUID.randomUUID().toString()}"
 
- podTemplate(name: 'kaniko', label: label, yaml: """
- kind: Pod
- metadata:
-   name: kaniko
- spec:
-   containers:
-   - name: kaniko
-     image: csanchez/kaniko:jenkins # we need a patched version of kaniko for now
-     imagePullPolicy: Always
-     command:
-     - cat
-     tty: true
-     volumeMounts:
-       - name: jenkins-docker-cfg
-         mountPath: /root
-   volumes:
-     - name: jenkins-docker-cfg
-       secret:
-         secretName: regcred
- """
-   ) {
+podTemplate(name: 'kaniko', label: label, yaml: """
+kind: Pod
+metadata:
+  name: kaniko
+spec:
+  containers:
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:debug
+    imagePullPolicy: Always
+    command:
+    - /busybox/cat
+    tty: true
+    volumeMounts:
+      - name: jenkins-docker-cfg
+        mountPath: /root
+  volumes:
+  - name: jenkins-docker-cfg
+    projected:
+      sources:
+      - secret:
+          name: regcred
+          items:
+            - key: .dockerconfigjson
+              path: .docker/config.json
+"""
+  ) {
 
-   node(label) {
-     stage('Build with Kaniko') {
-       git 'https://github.com/jenkinsci/docker-jnlp-slave.git'
-       container('kaniko') {
-           sh '/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure-skip-tls-verify --destination=mydockerregistry:5000/myorg/myimage'
-       }
-     }
-   }
- }
+  node(label) {
+    stage('Build with Kaniko') {
+      git 'https://github.com/jenkinsci/docker-jnlp-slave.git'
+      container(name: 'kaniko', shell: '/busybox/sh') {
+          sh '''#!/busybox/sh
+          /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure-skip-tls-verify --destination=mydockerregistry:5000/myorg/myimage
+          '''
+      }
+    }
+  }
+}
