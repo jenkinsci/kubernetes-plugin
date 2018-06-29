8734ef114e4247910eb24f3471a1e553013512a2
Andrew Bayer <andrew.bayer@gmail.com>
2018-06-29 09:46:02 -0400

    Skip checkout in the Declarative yamlfile test since the container can't access the localhost disk for the git repo

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/declarativeFromYamlFile.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/declarativeFromYamlFile.groovy
@@ -5,6 +5,10 @@ pipeline {
       yamlFile 'declarativeYamlFile.yml'
     }
   }
+  options {
+    // Because there's no way for the container to actually get at the git repo on the disk of the box we're running on.
+    skipDefaultCheckout(true)
+  }
   stages {
     stage('Run maven') {
       steps {
