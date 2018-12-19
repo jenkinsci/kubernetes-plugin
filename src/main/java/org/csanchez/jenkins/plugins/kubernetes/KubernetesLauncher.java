0c6d5c4e7753c5c3acda0dc0511801ae8ef02a00
Carlos Sanchez <carlos@apache.org>
2018-12-19 15:34:09 +0100

    Do not wait for agent connection if pod has failed
    Fix some logging

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesLauncher.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/KubernetesLauncher.java
@@ -122,6 +122,7 @@ public class KubernetesLauncher extends JNLPLauncher {
                 logger.printf("Waiting for Pod to be scheduled (%2$s/%3$s): %1$s%n", podId, waitedSec, waitForPodSec);
 
                 Thread.sleep(1000);
+                ++waitedSec;
                 pod = client.pods().inNamespace(namespace).withName(podId).get();
                 if (pod == null) {
                     throw new IllegalStateException("Pod no longer exists: " + podId);
@@ -165,7 +166,6 @@ public class KubernetesLauncher extends JNLPLauncher {
                     break;
                 }
 
-                ++waitedSec;
             } while (waitedSec < waitForPodSec);
             String status = pod.getStatus().getPhase();
             if (!validStates.contains(status)) {
@@ -174,9 +174,10 @@ public class KubernetesLauncher extends JNLPLauncher {
             }
 
             int waitForSlaveToConnect = unwrappedTemplate.getSlaveConnectTimeout();
+            int waitedForSlave;
 
             // now wait for agent to be online
-            for (int waitedForSlave = 0; waitedForSlave < waitForSlaveToConnect; waitedForSlave++) {
+            for (waitedForSlave = 0; waitedForSlave < waitForSlaveToConnect; waitedForSlave++) {
                 SlaveComputer slaveComputer = slave.getComputer();
                 if (slaveComputer == null) {
                     throw new IllegalStateException("Node was deleted, computer is null");
@@ -184,6 +185,17 @@ public class KubernetesLauncher extends JNLPLauncher {
                 if (slaveComputer.isOnline()) {
                     break;
                 }
+
+                // Check that the pod hasn't failed already
+                pod = client.pods().inNamespace(namespace).withName(podId).get();
+                if (pod == null) {
+                    throw new IllegalStateException("Pod no longer exists: " + podId);
+                }
+                status = pod.getStatus().getPhase();
+                if (!validStates.contains(status)) {
+                    break;
+                }
+
                 LOGGER.log(INFO, "Waiting for agent to connect ({1}/{2}): {0}",
                         new Object[]{podId, waitedForSlave, waitForSlaveToConnect});
                 logger.printf("Waiting for agent to connect (%2$s/%3$s): %1$s%n",
@@ -193,7 +205,7 @@ public class KubernetesLauncher extends JNLPLauncher {
             if (slave.getComputer() == null || slave.getComputer().isOffline()) {
                 logLastLines(containerStatuses, podId, namespace, slave, null, client);
                 throw new IllegalStateException(
-                        "Agent is not connected after " + waitForSlaveToConnect + " seconds, status: " + status);
+                        "Agent is not connected after " + waitedForSlave + " seconds, status: " + status);
             }
             computer.setAcceptingTasks(true);
         } catch (Throwable ex) {
