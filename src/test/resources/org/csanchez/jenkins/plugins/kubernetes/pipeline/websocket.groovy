5e30c244e13fb813d4a334cdb9fb4d14272b576d
Jesse Glick <jglick@cloudbees.com>
2019-07-02 09:31:01 -0400

    Forgot to use $NAME!

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/websocket.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/websocket.groovy
@@ -1,7 +1,7 @@
-podTemplate(label: 'websocket', containers: [
+podTemplate(label: '$NAME', containers: [
         containerTemplate(name: 'busybox', image: 'busybox', ttyEnabled: true, command: '/bin/cat'),
     ]) {
-    node('websocket') {
+    node('$NAME') {
         container('busybox') {
             sh 'sleep 5; echo have started user process; sleep 999'
         }
