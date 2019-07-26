7ce52ebe23db8bba38f830242a0dbe994ec0163e
Vincent Latombe <vincent@latombe.net>
2019-07-26 17:55:28 +0200

    Fix reviews from @jglick

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/showRawYamlFalseInherited.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/showRawYamlFalseInherited.groovy
@@ -1,9 +1,6 @@
-podTemplate(showRawYaml: false) { podTemplate(yaml: """
+podTemplate(showRawYaml: false) { podTemplate(yaml: '''
 apiVersion: v1
 kind: Pod
-metadata:
-  labels:
-    some-label: some-label-value
 spec:
   containers:
   - name: busybox
@@ -14,12 +11,7 @@ spec:
     env:
     - name: CONTAINER_ENV_VAR
       value: container-env-var-value
-    - name: CONTAINER_ENV_VAR_FROM_SECRET
-      valueFrom:
-        secretKeyRef:
-          key: password
-          name: container-secret
-"""
+'''
 )
 {
     node(POD_LABEL) {
