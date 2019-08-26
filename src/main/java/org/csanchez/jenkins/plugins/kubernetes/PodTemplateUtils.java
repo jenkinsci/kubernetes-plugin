e86236c3747162df1adaac348148a5179fc00451
eefrat <elhay.efrat@intel.com>
2019-08-26 15:52:45 +0300

    cahnge test and fixing small bug
    Signed-off-by: eefrat <elhay.efrat@intel.com>

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/PodTemplateUtils.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/PodTemplateUtils.java
@@ -92,7 +92,7 @@ public class PodTemplateUtils {
         String args = Strings.isNullOrEmpty(template.getArgs()) ? parent.getArgs() : template.getArgs();
         boolean ttyEnabled = template.isTtyEnabled() ? template.isTtyEnabled() : (parent.isTtyEnabled() ? parent.isTtyEnabled() : false);
         String resourceRequestCpu = Strings.isNullOrEmpty(template.getResourceRequestCpu()) ? parent.getResourceRequestCpu() : template.getResourceRequestCpu();
-        Long runAsUser = parent.getRunAsUser()  ==  Integer.toUnsignedLong(10000) ? parent.getRunAsUser() : template.getRunAsUser();
+        Long runAsUser = parent.getRunAsUser() != null && parent.getRunAsUser()  ==  Integer.toUnsignedLong(10000) ? parent.getRunAsUser() : template.getRunAsUser();
         String resourceRequestMemory = Strings.isNullOrEmpty(template.getResourceRequestMemory()) ? parent.getResourceRequestMemory() : template.getResourceRequestMemory();
         String resourceLimitCpu = Strings.isNullOrEmpty(template.getResourceLimitCpu()) ? parent.getResourceLimitCpu() : template.getResourceLimitCpu();
         String resourceLimitMemory = Strings.isNullOrEmpty(template.getResourceLimitMemory()) ? parent.getResourceLimitMemory() : template.getResourceLimitMemory();
@@ -141,9 +141,12 @@ public class PodTemplateUtils {
                 : (parent.getSecurityContext() != null ? parent.getSecurityContext().getPrivileged() : Boolean.FALSE);
         String imagePullPolicy = Strings.isNullOrEmpty(template.getImagePullPolicy()) ? parent.getImagePullPolicy()
                 : template.getImagePullPolicy();
-        Long runAsUser = template.getSecurityContext().getRunAsUser() == Integer.toUnsignedLong(10000) ? parent.getSecurityContext().getRunAsUser() : template.getSecurityContext().getRunAsUser();
+        Long runAsUser =  template.getSecurityContext() != null && template.getSecurityContext().getRunAsUser() != null
+                ?template.getSecurityContext().getRunAsUser()
+                : (parent.getSecurityContext() != null  ? parent.getSecurityContext().getRunAsUser() : Long.parseLong("10000"));
 
-        String workingDir = Strings.isNullOrEmpty(template.getWorkingDir())
+
+                String workingDir = Strings.isNullOrEmpty(template.getWorkingDir())
                 ? (Strings.isNullOrEmpty(parent.getWorkingDir()) ? DEFAULT_WORKING_DIR : parent.getWorkingDir())
                 : template.getWorkingDir();
         List<String> command = template.getCommand() == null ? parent.getCommand() : template.getCommand();
