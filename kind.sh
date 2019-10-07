d9bed78b8fa069b1f49ae82214fc6e27f8d56e05
Jesse Glick <jglick@cloudbees.com>
2019-10-07 12:06:48 -0400

    Need to install kubectl too.

--- kind.sh
+++ kind.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
-curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
-chmod +x $WSTMP/kind 
 export PATH=$WSTMP:$PATH
+curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
+chmod +x $WSTMP/kind
 kind create cluster --name $BUILD_TAG
 trap "kind delete cluster --name $BUILD_TAG" EXIT
 export KUBECONFIG="$(kind get kubeconfig-path --name $BUILD_TAG)"
+curl -Lo $WSTMP/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x $WSTMP/kubectl
 kubectl cluster-info
 kubectl run hi --quiet --rm --attach --restart=Never --image=busybox  -- sh -c 'echo hello world'
 kind export logs $WSTMP/kindlogs
