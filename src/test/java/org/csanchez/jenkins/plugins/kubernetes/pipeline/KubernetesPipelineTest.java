5e9c1813958f9a5c632bd95c3465af7383c162d6
Jesse Glick <jglick@cloudbees.com>
2019-10-24 16:03:40 -0400

    Launcher.kill in ContainerExecDecorator does not yet work in Windows.

--- src/test/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesPipelineTest.java
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesPipelineTest.java
@@ -63,6 +63,7 @@ import org.jvnet.hudson.test.LoggerRule;
 
 import hudson.model.Result;
 import java.util.Locale;
+import org.junit.Ignore;
 import org.jvnet.hudson.test.MockAuthorizationStrategy;
 
 /**
@@ -501,6 +502,17 @@ public class KubernetesPipelineTest extends AbstractKubernetesPipelineTest {
         r.assertLogContains("Directory of C:\\home\\jenkins\\agent\\workspace\\windows Container\\subdir", b);
         r.assertLogContains("C:\\Users\\ContainerAdministrator", b);
         r.assertLogContains("got stuff: some value", b);
+    }
+
+    @Ignore("TODO aborts, but with “kill finished with exit code 9009” and “After 20s process did not stop” and no graceful shutdown")
+    @Test
+    public void interruptedPodWindows() throws Exception {
+        assumeWindows();
+        cloud.setDirectConnection(false);
+        r.waitForMessage("starting to sleep", b);
+        b.getExecutor().interrupt();
+        r.assertBuildStatus(Result.ABORTED, r.waitForCompletion(b));
+        r.assertLogContains("shut down gracefully", b);
     }
 
     @Test
---
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/interruptedPodWindows.groovy
@@ -0,0 +1,23 @@
+podTemplate(yaml: '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: jnlp
+    image: jenkins/jnlp-agent:latest-windows
+  - name: shell
+    image: mcr.microsoft.com/powershell:preview-windowsservercore-1809
+    command:
+    - powershell
+    args:
+    - Start-Sleep
+    - 999999
+  nodeSelector:
+    beta.kubernetes.io/os: windows
+''') {
+    node(POD_LABEL) {
+        container('shell') {
+            powershell 'try {Write-Host starting to sleep; Start-Sleep 999999} finally {Write-Host shut down gracefully}'
+        }
+    }
+}
