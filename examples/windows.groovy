d3eaab02c8f36f7cda7490eb17e4880879161d81
Jesse Glick <jglick@cloudbees.com>
2019-10-25 08:33:34 -0400

    Adding example of Windows build.

---
+++ examples/windows.groovy
@@ -0,0 +1,27 @@
+/*
+ * Runs a build on a Windows pod.
+ * Tested in EKS: https://docs.aws.amazon.com/eks/latest/userguide/windows-support.html
+ */
+podTemplate(yaml: '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: jnlp
+    image: jenkins/jnlp-agent:latest-windows
+  - name: shell
+    image: mcr.microsoft.com/powershell:preview-windowsservercore-1809
+    command:
+    - powershell
+    args:
+    - Start-Sleep
+    - 999999
+  nodeSelector:
+    beta.kubernetes.io/os: windows
+''') {
+    node(POD_LABEL) {
+        container('shell') {
+            powershell 'Get-ChildItem Env: | Sort Name'
+        }
+    }
+}
