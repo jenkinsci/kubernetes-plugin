045e7c0b55a6cbd55951aafc98a0f359f1d5a441
Jesse Glick <jglick@cloudbees.com>
2019-11-15 12:51:34 -0500

    Try again, this time running only in-cluster tests.

--- test-in-k8s.sh
+++ test-in-k8s.sh
@@ -12,7 +12,7 @@ kubectl cp src jenkins:/checkout/src
 kubectl cp settings-azure.xml jenkins:/settings-azure.xml
 if [ -v TEST ]
 then
-    args="-Dtest=$TEST test"
+    args="-Dtest=$TEST -Dmaven.test.redirectTestOutputToFile test"
 else
     args=verify
 fi
