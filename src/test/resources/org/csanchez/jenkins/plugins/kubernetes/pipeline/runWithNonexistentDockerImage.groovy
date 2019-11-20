01f22a9efad99b6eb3f0a13cbeda23692c97e824
Pierson Yieh <yieh.pierson@gmail.com>
2019-11-20 14:43:19 -0800

    fixed groovy file?

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/runWithNonexistentDockerImage.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/runWithNonexistentDockerImage.groovy
@@ -1,30 +1,30 @@
 pipeline {
-    agent {
-        kubernetes {
-            label "somelabel-${UUID.randomUUID().toString()}"
-            defaultContainer "jnlpdocker"
-            yaml """
-         apiVersion: v1
-         kind: Pod
-         spec:
-            containers:
-            - name: container1
-              image: nonexistent-docker-image
-              command:
-              - cat
-              tty: true
-        """
+  agent {
+    kubernetes {
+      label "somelabel-${UUID.randomUUID().toString()}"
+      defaultContainer "jnlpdocker"
+      yaml """
+apiVersion: v1
+kind: Pod
+spec:
+containers:
+- name: container1
+  image: nonexistent-docker-image
+  command:
+  - cat
+  tty: true
+"""
+     }
+  }
+  stages {
+    stage('Run') {
+      steps {
+        container('container1') {
+          sh """
+        will never run
+          """
         }
+      }
     }
-    stages {
-        stage('Run') {
-            steps {
-                container('container1') {
-                    sh """
-                    will never run
-                  """
-                }
-            }
-        }
-    }
+  }
 }
