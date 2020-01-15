7bd8a26ed1af82bcfd605fce78594c299809c30d
Jesse Glick <jglick@cloudbees.com>
2020-01-15 15:33:22 -0500

    Use enhanced PV support in Kind 0.7.0

--- kind.sh
+++ kind.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 export PATH=$WSTMP:$PATH
 if [ \! -f $WSTMP/kind ]
 then
-    curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
+    curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
     chmod +x $WSTMP/kind
 fi
 if [ \! -f $WSTMP/kubectl ]
@@ -23,11 +23,6 @@ function cleanup() {
 trap cleanup EXIT
 export KUBECONFIG="$(kind get kubeconfig-path --name $cluster)"
 kubectl cluster-info
-
-# https://github.com/kubernetes-sigs/kind/issues/118#issuecomment-475134086
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/205a80824bc6ea6095602458c15651f58146eae4/deploy/local-path-storage.yaml
-kubectl annotate storageclass local-path storageclass.beta.kubernetes.io/is-default-class=true
-kubectl delete storageclass standard
 
 bash test-in-k8s.sh
 rm -rf $WSTMP/surefire-reports
