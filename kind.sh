b9c2774d37b86f3d974044b217c1d7274840b77b
Vincent Latombe <vincent@latombe.net>
2020-01-17 14:22:56 +0100

    Kind improvements
    * kubectl 1.17.0
    * pick up OS distribution depending on where the script runs
    * pull image beforehand then load it in kind

--- kind.sh
+++ kind.sh
@@ -4,25 +4,29 @@ set -euxo pipefail
 export PATH=$WSTMP:$PATH
 if [ \! -f $WSTMP/kind ]
 then
-    curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
+    curl -Lo $WSTMP/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname | tr '[:upper:]' '[:lower:]')-amd64
     chmod +x $WSTMP/kind
 fi
 if [ \! -f $WSTMP/kubectl ]
 then
-    curl -Lo $WSTMP/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl
+    curl -Lo $WSTMP/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/$(uname | tr '[:upper:]' '[:lower:]')/amd64/kubectl
     chmod +x $WSTMP/kubectl
 fi
 
-# TODO use kind load to take better advantage of images cached in the host VM
 export cluster=ci$RANDOM
 export KUBECONFIG=$WSTMP/kubeconfig-$cluster
 kind create cluster --name $cluster --wait 5m
 function cleanup() {
     kind export logs --name $cluster $WSTMP/kindlogs || :
     kind delete cluster --name $cluster || :
+    rm $KUBECONFIG
 }
 trap cleanup EXIT
 kubectl cluster-info
+
+DOCKER_IMAGE=$(grep -e image: test-in-k8s.yaml | cut -d ':' -f 2- | xargs)
+docker pull $DOCKER_IMAGE
+kind load docker-image $DOCKER_IMAGE --name $cluster
 
 bash test-in-k8s.sh
 rm -rf $WSTMP/surefire-reports
