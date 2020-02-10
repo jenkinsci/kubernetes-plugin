e8afb8a4a06e3ca033c427767606ebc5e316cd01
Jesse Glick <jglick@cloudbees.com>
2020-02-10 12:52:27 -0500

    Also add archiveArtifacts to Maven sample

--- src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/samples/maven.groovy
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/samples/maven.groovy
@@ -56,5 +56,6 @@ public class SomeTest {
             sh 'mvn -B -ntp -Dmaven.test.failure.ignore verify'
         }
         junit '**/target/surefire-reports/TEST-*.xml'
+        archiveArtifacts '**/target/*.jar'
     }
 }
