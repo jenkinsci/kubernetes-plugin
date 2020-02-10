493413d1a5dc311bb940a8c5c8f637071d9d1575
Jesse Glick <jglick@cloudbees.com>
2020-02-10 11:28:19 -0500

    Using GroovySample extension point

---
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/samples/maven.groovy
@@ -0,0 +1,57 @@
+// Build a trivial Maven project using the standard image.
+podTemplate(yaml: '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: maven
+    image: maven:3.6.3-jdk-8
+    command:
+    - sleep
+    args:
+    - infinity
+''') {
+    node(POD_LABEL) {
+        writeFile file: 'pom.xml', text: '''
+<project xmlns="http://maven.apache.org/POM/4.0.0">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>sample</groupId>
+    <artifactId>sample</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-surefire-plugin</artifactId>
+                <version>2.18.1</version>
+            </plugin>
+        </plugins>
+    </build>
+    <dependencies>
+        <dependency>
+            <groupId>junit</groupId>
+            <artifactId>junit</artifactId>
+            <version>4.12</version>
+            <scope>test</scope>
+        </dependency>
+    </dependencies>
+    <properties>
+        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+        <maven.compiler.source>1.8</maven.compiler.source>
+        <maven.compiler.target>1.8</maven.compiler.target>
+    </properties>
+</project>
+        '''
+        writeFile file: 'src/test/java/sample/SomeTest.java', text: '''
+package sample;
+public class SomeTest {
+    @org.junit.Test
+    public void checks() {}
+}
+        '''
+        container('maven') {
+            sh 'mvn -B -ntp -Dmaven.test.failure.ignore verify'
+        }
+        junit '**/target/surefire-reports/TEST-*.xml'
+    }
+}
---
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesSamplesTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 CloudBees, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.csanchez.jenkins.plugins.kubernetes.pipeline;
+
+import hudson.ExtensionList;
+import static org.csanchez.jenkins.plugins.kubernetes.KubernetesTestUtil.deletePods;
+import static org.csanchez.jenkins.plugins.kubernetes.KubernetesTestUtil.getLabels;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.cps.GroovySample;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+public class KubernetesSamplesTest extends AbstractKubernetesPipelineTest {
+
+    // TODO tried without success to use Parameterized here (need to construct parameters _after_ JenkinsRule starts)
+    @Rule public ErrorCollector errors = new ErrorCollector();
+
+    @Before public void setUp() throws Exception {
+        deletePods(cloud.connect(), getLabels(cloud, this, name), false);
+    }
+
+    @Test public void smokes() throws Exception {
+        for (GroovySample gs : ExtensionList.lookup(GroovySample.class)) {
+            System.err.println("==== " + gs.title() + " ====");
+            p = r.createProject(WorkflowJob.class, gs.name());
+            p.setDefinition(new CpsFlowDefinition(gs.script(), true));
+            errors.checkSucceeds(() -> r.buildAndAssertSuccess(p));
+        }
+    }
+
+}
