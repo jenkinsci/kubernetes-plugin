b0737cdab4938b4cd84891c47ae342db438a1dec
Vincent Latombe <vincent@latombe.net>
2020-12-11 18:15:53 +0100

    Use an existing secret

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/octalPermissions.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/octalPermissions.groovy
@@ -2,9 +2,14 @@ podTemplate(yaml: '''
 spec:
   volumes:
   - name: jenkins-agent
-    configMap:
-      name: jenkins-agent
-      defaultMode: 0777
+    projected:
+      sources:
+      - secret:
+          name: container-secret
+          items:
+            - key: password
+              path: my-group/my-password
+              mode: 0777
 ''') {
   node(POD_LABEL){
     sh 'true'
