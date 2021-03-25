23af3c02543ef6b8f0602e75ddc09f2e440dff03
Marc Peremiquel <marc.peremiquel@ticxcat.cat>
2021-03-25 16:28:03 +0100

    add showRawYaml

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgent.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgent.java
@@ -73,6 +73,8 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
     private String yaml;
     @CheckForNull
     private String yamlFile;
+    @CheckForNull
+    private Boolean showRawYaml;
     private YamlMergeStrategy yamlMergeStrategy;
     @CheckForNull
     private WorkspaceVolume workspaceVolume;
@@ -272,6 +274,15 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
     }
 
     @DataBoundSetter
+    public void setShowRawYaml(Boolean showRawYaml) {
+        this.showRawYaml = showRawYaml;
+    }
+
+    public Boolean getShowRawYaml() {
+        return showRawYaml;
+    }
+
+    @DataBoundSetter
     public void setYamlFile(String yamlFile) {
         this.yamlFile = yamlFile;
     }
@@ -327,6 +338,9 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
         if (!StringUtils.isEmpty(yaml)) {
             argMap.put("yaml", yaml);
         }
+        if (showRawYaml != null) {
+            argMap.put("showRawYaml", showRawYaml);
+        }
         if (yamlMergeStrategy != null) {
             argMap.put("yamlMergeStrategy", yamlMergeStrategy);
         }
@@ -377,7 +391,7 @@ public class KubernetesDeclarativeAgent extends DeclarativeAgent<KubernetesDecla
     @Symbol("kubernetes")
     public static class DescriptorImpl extends DeclarativeAgentDescriptor<KubernetesDeclarativeAgent> {
 
-        static final String[] POD_TEMPLATE_FIELDS = {"namespace", "inheritFrom", "yaml", "instanceCap", "podRetention", "supplementalGroups", "idleMinutes", "activeDeadlineSeconds", "serviceAccount", "nodeSelector", "workingDir", "workspaceVolume"};
+        static final String[] POD_TEMPLATE_FIELDS = {"namespace", "inheritFrom", "yaml", "showRawYaml", "instanceCap", "podRetention", "supplementalGroups", "idleMinutes", "activeDeadlineSeconds", "serviceAccount", "nodeSelector", "workingDir", "workspaceVolume"};
 
         public DescriptorImpl() {
             for (String field: new String[] {"cloud", "label"}) {
