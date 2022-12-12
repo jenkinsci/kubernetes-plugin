ae5f38299cd5e8cf43c8538a304c659d32caa518
Jesse Glick <jglick@cloudbees.com>
2022-12-12 17:15:44 -0500

    Fixing `KubernetesPipelineTest.cascadingDelete` which seems to have broken somehow by indentation

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/cascadingDelete.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/cascadingDelete.groovy
@@ -7,14 +7,11 @@ kind: Pod
 spec:
   containers:
     - name: kubectl
-      image: bitnami/kubectl:1.12
-      stdin: true
-      tty: true
+      image: bitnami/kubectl:1.25.4
       command:
-        - '/bin/sh'
+        - sleep
       args:
-        - -c
-        - cat
+        - infinity
       env:
         - name: HUB_NAME
           valueFrom:
@@ -35,48 +32,46 @@ spec:
 ''') {
     node(POD_LABEL) {
         container('kubectl') {
-            sh('''\
-                deploy=$(
-                kubectl create -o name -f - <<EOF
-                apiVersion: apps/v1
-                kind: Deployment
-                metadata:
-                  name: cascading-delete
-                  labels:
-                    app: cascading-delete
-                  ownerReferences:
-                  - apiVersion: v1
-                    kind: Pod
-                    controller: true
-                    blockOwnerDeletion: true
-                    name: $HUB_NAME
-                    uid: $HUB_UID
-                spec:
-                  revisionHistoryLimit: 0
-                  replicas: 2
-                  selector:
-                    matchLabels:
-                      app: cascading-delete
-                  template:
-                    metadata:
-                      labels:
-                        app: cascading-delete
-                    spec:
-                      terminationGracePeriodSeconds: 0
-                      containers:
-                      - name: ubuntu
-                        image: ubuntu
-                        command:
-                          - '/bin/sh'
-                        args:
-                          - -c
-                          - 'sleep infinity'
-                      nodeSelector:
-                        kubernetes.io/os: linux
-                EOF
-                )
-                kubectl rollout status "$deploy"
-            '''.stripIndent())
+            sh '''
+kubectl create -f - <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: cascading-delete
+  labels:
+    app: cascading-delete
+  ownerReferences:
+  - apiVersion: v1
+    kind: Pod
+    controller: true
+    blockOwnerDeletion: true
+    name: $HUB_NAME
+    uid: $HUB_UID
+spec:
+  revisionHistoryLimit: 0
+  replicas: 2
+  selector:
+    matchLabels:
+      app: cascading-delete
+  template:
+    metadata:
+      labels:
+        app: cascading-delete
+    spec:
+      terminationGracePeriodSeconds: 0
+      containers:
+      - name: ubuntu
+        image: ubuntu
+        command:
+          - '/bin/sh'
+        args:
+          - -c
+          - 'sleep infinity'
+      nodeSelector:
+        kubernetes.io/os: linux
+EOF
+kubectl rollout status deployment cascading-delete
+'''
         }
     }
     node(POD_LABEL) {
