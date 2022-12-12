a8b1ec0910b55bce96858fc8c5e5b3b8624683c7
Jesse Glick <jglick@cloudbees.com>
2022-12-12 17:39:38 -0500

    `KubernetesPipelineTest.jnlpWorkingDir` was also using an obsolete agent image

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/jnlpWorkingDir.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/jnlpWorkingDir.groovy
@@ -1,5 +1,5 @@
 podTemplate(containers: [
-    containerTemplate(name: 'jnlp', image: 'jenkinsci/jnlp-slave:latest', workingDir: '/home/jenkins'),
+    containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:3077.vd69cf116da_6f-3-jdk11', workingDir: '/home/jenkins'),
     containerTemplate(name: 'busybox', image: 'busybox', ttyEnabled: true, command: '/bin/cat')
 ]) {
     node (POD_LABEL) {
