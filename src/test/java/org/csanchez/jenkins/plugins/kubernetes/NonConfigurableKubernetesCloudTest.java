3bdeb1952b38514a8215131bb83278b8cc1bbadb
Thorsten Scherler <scherler@gmail.com>
2024-01-03 14:32:58 +0100

    [fix_non_config_instance] add more tests
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- src/test/java/org/csanchez/jenkins/plugins/kubernetes/NonConfigurableKubernetesCloudTest.java
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/NonConfigurableKubernetesCloudTest.java
@@ -1,5 +1,7 @@
 package org.csanchez.jenkins.plugins.kubernetes;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Rule;
@@ -28,14 +30,15 @@ public class NonConfigurableKubernetesCloudTest {
         jenkins.clouds.add(cloud);
         jenkins.save();
         var readOnly = new NonConfigurableKubernetesCloud("NonConfigurableKubernetes", cloud);
-
+        readOnly.removeTemplate(podTemplate); // should not remove anything
         var podTemplate2 = new PodTemplate();
         podTemplate2.setName("test-template-read-only");
         podTemplate2.setLabel("test-read-only");
         readOnly.addTemplate(podTemplate2);
         jenkins.clouds.add(readOnly);
         jenkins.save();
-        // check that the read only does not added the template
+        assertEquals(2, jenkins.clouds.size()); // sanity check
+        assertEquals(1, readOnly.getTemplates().size()); // remove should not have removed anything, nor should have add
         readOnly.getTemplates().forEach(t -> {
             if (t.getName().equals("test-template-read-only")) {
                 throw new RuntimeException("Template should not be added");
