a54b26a175b3b521e1c641b3681cc83e1468a124
Thorsten Scherler <scherler@gmail.com>
2024-01-03 13:52:15 +0100

    [fix_non_config_instance] add unimplemented methods for non config classes
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

---
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/NonConfigurableKubernetesCloudTest.java
@@ -0,0 +1,45 @@
+package org.csanchez.jenkins.plugins.kubernetes;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.LoggerRule;
+
+public class NonConfigurableKubernetesCloudTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Rule
+    public LoggerRule logs = new LoggerRule().record(Logger.getLogger(NonConfigurableKubernetesCloudTest.class.getPackage().getName()),
+            Level.ALL);
+
+    @Test
+    public void configRoundTrip() throws Exception {
+        // create a cloud with a template
+        var cloud = new KubernetesCloud("kubernetes");
+        var podTemplate = new PodTemplate();
+        podTemplate.setName("test-template");
+        podTemplate.setLabel("test");
+        cloud.addTemplate(podTemplate);
+        var jenkins = j.jenkins;
+        jenkins.clouds.add(cloud);
+        jenkins.save();
+        var readOnly = new NonConfigurableKubernetesCloud("NonConfigurableKubernetes", cloud);
+        podTemplate.setName("test-template-read-only");
+        podTemplate.setLabel("test-read-only");
+        readOnly.addTemplate(podTemplate);
+        jenkins.clouds.add(readOnly);
+        jenkins.save();
+        // check that the read only does not added the template
+        readOnly.getTemplates().forEach(t -> {
+            if (t.getName().equals("test-template-read-only")) {
+                throw new RuntimeException("Template should not be added");
+            }
+        });
+
+    }
+
+}
