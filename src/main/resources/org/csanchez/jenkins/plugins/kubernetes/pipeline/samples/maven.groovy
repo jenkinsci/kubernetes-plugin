938e2b9c2ecb9834b9c66f11d7fa3bc8769799e0
Vincent Latombe <vincent@latombe.net>
2024-05-23 11:04:53 +0200

    Remove blank hunk

--- src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/samples/maven.groovy
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/samples/maven.groovy
@@ -1,7 +1,6 @@
 // Build a Maven project using the standard image and Scripted syntax.
 // Rather than inline YAML, you could use: yaml: readTrusted('jenkins-pod.yaml')
 // Or, to avoid YAML: containers: [containerTemplate(name: 'maven', image: 'maven:3.6.3-jdk-8', command: 'sleep', args: 'infinity')]
-
 podTemplate(yaml: '''
 apiVersion: v1
 kind: Pod
