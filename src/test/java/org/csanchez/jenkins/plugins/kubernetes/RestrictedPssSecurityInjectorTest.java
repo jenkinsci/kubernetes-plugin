4018ef228d45ec80f05cf11496a283e7cc963a32
Vincent Latombe <vincent@latombe.net>
2024-06-06 13:53:19 +0200

    Apply restricted PSS to all containers when enabled
    When restrictedPssSecurityContext = true in a kubernetes cloud, all created containers will be augmented with a security context compatible with "restricted" Pod Security Admission.
    
    If a security context has been manually set earlier, this won't be overridden. In such case, the pod will be rejected and the user should correct its pod definition.

---
+++ src/test/java/org/csanchez/jenkins/plugins/kubernetes/RestrictedPssSecurityInjectorTest.java
@@ -0,0 +1,28 @@
+package org.csanchez.jenkins.plugins.kubernetes;
+
+import java.io.IOException;
+import org.csanchez.jenkins.plugins.kubernetes.pod.decorator.PodDecorator;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestrictedPssSecurityInjectorTest extends AbstractGoldenFileTest {
+    @Before
+    public void configureCloud() {
+        cloud.setRestrictedPssSecurityContext(true);
+    }
+
+    @Override
+    protected PodDecorator newDecorator() {
+        return new RestrictedPssSecurityContextInjector();
+    }
+
+    @Test
+    public void simple() throws IOException {
+        test("simple");
+    }
+
+    @Test
+    public void multiContainer() throws IOException {
+        test("multiContainer");
+    }
+}
