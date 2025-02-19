1a7c46e5faa5a817aff2b9f31ceb9dbc1524c997
James Nord <jtnord@users.noreply.github.com>
2025-02-19 15:37:37 +0000

    use more recent image to suppoer newer cores
    fails in the PCT e.g. 
    
    ```
    -----Logs-------------
    Feb 18, 2025 5:12:08 PM hudson.remoting.jnlp.Main createEngine
    INFO: Setting up agent: jnlp-working-dir-1-w1k7d-j42j3-xc6qg
    Feb 18, 2025 5:12:08 PM hudson.remoting.Engine startEngine
    INFO: Using Remoting version: 3077.vd69cf116da_6f
    Feb 18, 2025 5:12:08 PM org.jenkinsci.remoting.engine.WorkDirManager initializeWorkDir
    INFO: Using /home/jenkins/remoting as a remoting work directory
    Feb 18, 2025 5:12:08 PM org.jenkinsci.remoting.engine.WorkDirManager setupLogging
    INFO: Both error and output logs will be printed to /home/jenkins/remoting
    Feb 18, 2025 5:12:08 PM hudson.remoting.jnlp.Main$CuiListener status
    INFO: Locating server among [http://jenkins.default:8000/jenkins/]
    Feb 18, 2025 5:12:08 PM hudson.remoting.jnlp.Main$CuiListener error
    SEVERE: Agent version 3107.v665000b_51092 or newer is required.
    java.io.IOException: Agent version 3107.v665000b_51092 or newer is required.
    ```

--- src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/jnlpWorkingDir.groovy
+++ src/test/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/jnlpWorkingDir.groovy
@@ -1,5 +1,5 @@
 podTemplate(containers: [
-    containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:3283.v92c105e0f819-7', workingDir: '/home/jenkins'),
+    containerTemplate(name: 'jnlp', image: 'jenkins/inbound-agent:0a4c2cf1212134f2179911ca9d00ef70a5435b16e361cd7e9a1c19489492bfdc', workingDir: '/home/jenkins'),
     containerTemplate(name: 'busybox', image: 'busybox', ttyEnabled: true, command: '/bin/cat')
 ]) {
     node (POD_LABEL) {
