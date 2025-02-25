dc610c6ca9aca3fec21dce0973485829251d4cb0
Jesse Glick <jglick@cloudbees.com>
2025-02-25 13:17:50 -0500

    Avoid excessive `Closure`s

--- src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgentScript.groovy
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesDeclarativeAgentScript.groovy
@@ -24,62 +24,58 @@
 package org.csanchez.jenkins.plugins.kubernetes.pipeline
 
 import org.jenkinsci.plugins.pipeline.modeldefinition.agent.CheckoutScript
-import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgentScript
+import org.jenkinsci.plugins.pipeline.modeldefinition.agent.DeclarativeAgentScript2
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-public class KubernetesDeclarativeAgentScript extends DeclarativeAgentScript<KubernetesDeclarativeAgent> {
+public class KubernetesDeclarativeAgentScript extends DeclarativeAgentScript2<KubernetesDeclarativeAgent> {
     public KubernetesDeclarativeAgentScript(CpsScript s, KubernetesDeclarativeAgent a) {
         super(s, a)
     }
 
     @Override
-    public Closure run(Closure body) {
-        return {
-            if ((describable.getYamlFile() != null) && (describable.hasScmContext(script))) {
-                describable.setYaml(script.readTrusted(describable.getYamlFile()))
-            }
-            if (describable.getInheritFrom() == null) {
-                // Do not implicitly inherit from parent template context for declarative Kubernetes agent declaration
-                describable.setInheritFrom("")
-            }
-            if (describable.labelExpression != null) {
-                script.echo '[WARNING] label option is deprecated. To use a static pod template, use the \'inheritFrom\' option.'
-            }
-            if (describable.containerTemplate != null) {
-                script.echo '[WARNING] containerTemplate option is deprecated, use yaml syntax to define containers.'
-            }
-            script.podTemplate(describable.asArgs) {
-                Closure run = {
-                    script.node(describable.labelExpression ?: script.POD_LABEL) {
-                        CheckoutScript.doCheckout(script, describable, describable.customWorkspace) {
-                            // what container to use for the main body
-                            def container = describable.defaultContainer ?: 'jnlp'
+    public void run(Closure body) {
+        if ((describable.getYamlFile() != null) && (describable.hasScmContext(script))) {
+            describable.setYaml(script.readTrusted(describable.getYamlFile()))
+        }
+        if (describable.getInheritFrom() == null) {
+            // Do not implicitly inherit from parent template context for declarative Kubernetes agent declaration
+            describable.setInheritFrom("")
+        }
+        if (describable.labelExpression != null) {
+            script.echo '[WARNING] label option is deprecated. To use a static pod template, use the \'inheritFrom\' option.'
+        }
+        if (describable.containerTemplate != null) {
+            script.echo '[WARNING] containerTemplate option is deprecated, use yaml syntax to define containers.'
+        }
+        script.podTemplate(describable.asArgs) {
+            Closure run = {
+                script.node(describable.labelExpression ?: script.POD_LABEL) {
+                    CheckoutScript.doCheckout2(script, describable, describable.customWorkspace) {
+                        // what container to use for the main body
+                        def container = describable.defaultContainer ?: 'jnlp'
 
-                            if (describable.containerTemplate != null) {
-                                // run inside the container declared for backwards compatibility
-                                container = describable.containerTemplate.asArgs
-                            }
+                        if (describable.containerTemplate != null) {
+                            // run inside the container declared for backwards compatibility
+                            container = describable.containerTemplate.asArgs
+                        }
 
-                            // call the main body
-                            if (container == 'jnlp') {
-                                // If default container is not changed by the pipeline user,
-                                // do not enclose the body with a `container` statement.
-                                body.call()
-                            } else {
-                                script.container(container) {
-                                    body.call()
-                                }
-                            }
-                        }.call()
+                        // call the main body
+                        if (container == 'jnlp') {
+                            // If default container is not changed by the pipeline user,
+                            // do not enclose the body with a `container` statement.
+                            body.call()
+                        } else {
+                            script.container(container, body)
+                        }
                     }
                 }
-                if (describable.retries > 1) {
-                    script.retry(count: describable.retries, conditions: [script.kubernetesAgent(), script.nonresumable()]) {
-                        run.call()
-                    }
-                } else {
+            }
+            if (describable.retries > 1) {
+                script.retry(count: describable.retries, conditions: [script.kubernetesAgent(), script.nonresumable()]) {
                     run.call()
                 }
+            } else {
+                run.call()
             }
         }
     }
