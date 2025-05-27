f3d6c3938f1eae421afdf29e542373f7cfd28b23
Kyle Cronin <cronik@users.noreply.github.com>
2025-05-26 22:14:12 -0400

    Make KubernetesNodeContext public
    Refactor `KubernetesNodeContext` to make it public for extensions to make
    use of `ContainerExecDecorator`. This refactor introduces a new method
    `getPodResource()` to get access to the associated `PodResource` from
    the client. This reduces the need for direct access to `KubernetesClient`
    and the `connectToCloud()` method.

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesNodeContext.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/pipeline/KubernetesNodeContext.java
@@ -16,48 +16,89 @@
 
 package org.csanchez.jenkins.plugins.kubernetes.pipeline;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.AbortException;
 import hudson.model.Node;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.PodResource;
 import java.io.IOException;
 import java.io.Serializable;
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesSlave;
+import org.jenkinsci.plugins.kubernetes.auth.KubernetesAuthException;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 
 /**
  * helper class for steps running in a kubernetes `node` context
  */
-class KubernetesNodeContext implements Serializable {
+public class KubernetesNodeContext implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private StepContext context;
+    private final StepContext context;
+    private final String podName;
 
-    private String podName;
-    private String namespace;
+    @CheckForNull
+    private final String namespace;
 
-    KubernetesNodeContext(StepContext context) throws Exception {
+    /**
+     * Create new Kubernetes context
+     * @param context step context, not null
+     * @throws Exception if {@link Node} context not instance of {@link KubernetesSlave} or interrupted.
+     */
+    public KubernetesNodeContext(@NonNull StepContext context) throws Exception {
         this.context = context;
         KubernetesSlave agent = getKubernetesSlave();
         this.podName = agent.getPodName();
         this.namespace = agent.getNamespace();
     }
 
-    // TODO remove the Exception thrown
-    String getPodName() throws Exception {
+    /**
+     * Kubernetes Pod name.
+     * @return pod name, never {@code null}
+     */
+    @NonNull
+    public String getPodName() {
         return podName;
     }
 
-    // TODO remove the Exception thrown
-    public String getNamespace() throws Exception {
+    /**
+     * Kubernetes namespace Pod is running in.
+     * @return kubernetes namespace or {@code null}
+     */
+    @Nullable
+    public String getNamespace() {
         return namespace;
+    }
+
+    /**
+     * Get node {@link PodResource}.
+     * @return client pod resource, never {@code null}
+     * @throws IOException if IO exception
+     * @throws InterruptedException if interrupted
+     * @throws KubernetesAuthException if authentication failure
+     * @see org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud#getPodResource(String, String)
+     */
+    @NonNull
+    public PodResource getPodResource() throws IOException, InterruptedException, KubernetesAuthException {
+        return getKubernetesSlave().getKubernetesCloud().getPodResource(namespace, podName);
     }
 
     KubernetesClient connectToCloud() throws Exception {
         return getKubernetesSlave().getKubernetesCloud().connect();
     }
 
-    private KubernetesSlave getKubernetesSlave() throws IOException, InterruptedException {
+    /**
+     * Get {@link Node} from the {@link StepContext}. If the context instance is instance of
+     * {@link KubernetesSlave} it will be returned otherwise an exception is thrown.
+     * @return kubernetes slave node context, never {@code null}
+     * @throws IOException if IO exception
+     * @throws InterruptedException if interrupted
+     * @throws AbortException if {@link Node} context is not instance of {@link KubernetesSlave}
+     */
+    @NonNull
+    public final KubernetesSlave getKubernetesSlave() throws IOException, InterruptedException {
         Node node = context.get(Node.class);
         if (!(node instanceof KubernetesSlave)) {
             throw new AbortException(
